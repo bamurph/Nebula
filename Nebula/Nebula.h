364fcecf68cb84285936966021519bf8657900e3
Ben Murphy <ben@constellation.software>
2016-12-13 16:34:37 -0500

    Initial Commit

---
+++ Nebula/Nebula.h
@@ -0,0 +1,19 @@
+//
+//  Nebula.h
+//  Nebula
+//
+//  Created by Ben Murphy on 12/13/16.
+//  Copyright © 2016 Constellation Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nebula.
+FOUNDATION_EXPORT double NebulaVersionNumber;
+
+//! Project version string for Nebula.
+FOUNDATION_EXPORT const unsigned char NebulaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nebula/PublicHeader.h>
+
+
